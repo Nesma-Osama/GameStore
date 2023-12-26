e174d4e22400fc3f136311bb15db8e521ff0ef0e
Nesma Osamsa <120592246+NesmaOsama@users.noreply.github.com>
2023-12-27 01:33:53 +0200

    coupon with ad

--- controller/Ad.js
+++ controller/Ad.js
@@ -1,20 +1,67 @@
-const db=require('../connect/connection')
-exports.CreateAd=async(req,res)=>{
-const {Name,Image,CompanyName,Description,AdminE}=req.body
-let sql;
+const db = require('../connect/connection')
+exports.CreateAd = async (req, res) => {
+  const { Name, Image, CompanyName, Description, AdminE } = req.body
+  let sql;
 
-  sql=`insert into ad  values('${Name}','${Description}','${Image}','${CompanyName}','${AdminE}')`;
+  sql = `insert into ad  values('${Name}','${Description}','${Image}','${CompanyName}','${AdminE}')`;
 
-const result=await db.query(sql);
-const {affectedRows}=result[0];//decomposite the returned result
-if(affectedRows===1)
-res.send({iscreated:'created'})
-else
-res.send({iscreated:'Error when try to insert this ad'})
+  const result = await db.query(sql);
+  const { affectedRows } = result[0];//decomposite the returned result
+  if (affectedRows === 1)
+    res.send({ iscreated: 'created' })
+  else
+    res.send({ iscreated: 'Error when try to insert this ad' })
 }
 
-exports.GetAll=async(req,res)=>{
-const sql=`select Name,Description,Image,CompanyName from Ad`
-const result=await db.query(sql)
-res.send(result[0]);
+exports.GetAll = async (req, res) => {
+  const sql = `select Name,Description,Image,CompanyName from Ad`
+  const result = await db.query(sql)
+  res.send(result[0]);
+}
+exports.GetAd = async (req, res, next) => {
+  const { Name } = req.body
+  const sql = `select ad.Name,ad.Description,Image,CompanyName,Logo,Company.Description as CDescription from ad,company where ad.name='${Name}'`
+  const result = await db.query(sql);
+  res.send(result[0]);
+}
+
+exports.Watch = async (req, res, next) => {
+  const { Name, Email } = req.body
+  const sql = `insert into playerwatch values('${Email}','${Name}')`
+  const result = await db.query(sql)
+  const sql2 = `select count(* ) as count from player where (select watched from player where email='${Email}')%1=0;`
+  const result2 = await db.query(sql2);
+  const { count } = result2[0][0];
+  if (count === 1) {
+    const sql3 = `
+        SELECT coupid FROM coupon
+        where coupid not in (select couponid from hascoupon as coupon) and redeemed !=1
+        ORDER BY RAND()
+        LIMIT 1; `
+    const result3 = await db.query(sql3);
+    const ishascoupon = result3[0][0]
+    console.log(ishascoupon)
+    if (ishascoupon == null)
+      res.send('Thanks for your time i hope you liked our ad')
+
+    else {
+      const insert = `insert into hascoupon values('${Email}',${ishascoupon.coupid})`
+      const inserted = await db.query(insert)
+      const affect = inserted[0].affectedRows
+      console.log(affect)
+      if (affect === 1)
+        res.send('Congratulates You won a coupon please check your coupons')
+      else
+        res.send('Thanks for your time i hope you liked our ad')
+
+    }
+  }
+  else
+    res.send('Thanks for your time i hope you liked our ad')
+
+
+
+
+
+
 }
