11706b4e7faa55bbb7b4dd8d0ca73c4851fe7b95
Nesma Osamsa <120592246+NesmaOsama@users.noreply.github.com>
2023-12-27 17:35:27 +0200

    updateimage

--- app.js
+++ app.js
@@ -7,15 +7,16 @@ const PlayerRouter=require('./Routers/player')
 const game_router=require('./Routers/game')
 const companyRouter=require('./Routers/comany')
 const AdRouter=require('./Routers/Ad')
-var bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+
 ///connection part
 ///////////////////////////
 const cors=require("cors");
 app.use(cors());
+
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb' }));
 app.use(express.json());
-
-
-
 
 //////////////////////////////////
 app.use('/admin',adminRouter)
