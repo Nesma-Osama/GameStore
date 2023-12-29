629fedf850082cb3992047b8ee6567c5e11ffd0b
AhmedFouad23 <119745342+AhmedFouad23@users.noreply.github.com>
2023-12-29 06:15:09 +0200

    Update game.js

--- controller/game.js
+++ controller/game.js
@@ -23,7 +23,7 @@ added:"Failed to add game"});
 }
 exports.get_game_info=async(req,res,next)=>{
     const{name}=req.body;
-    let query=`Select Name, description, price, MIN_AGE , rdate ,AVELIABLEDELET , totalNumberSales,AVGRating,image,cemail from game where game.Name='${name}'`;
+    let query=`Select Name, description, price, MIN_AGE , rdate ,AVELIABLEDELET ,totalNumberSales,AVGRating,image,cemail from game where game.Name='${name}'`;
     const data=await db.query(query);
     res.send(data[0]);
 }
